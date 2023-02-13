b501ed103985ffb2cb8afc72cf3fafc274fcd031
dani_prayogi <dnrp_code@gmail.com>
2023-02-13 16:54:10 +0700

    init intergration firebase

--- lib/app/data/providers/product_provider.dart
+++ lib/app/data/providers/product_provider.dart
@@ -1,24 +1,25 @@
+import 'dart:ffi';
+
 import 'package:get/get.dart';
 
 import '../models/product_model.dart';
 
 class ProductProvider extends GetConnect {
-  @override
-  void onInit() {
-    httpClient.defaultDecoder = (map) {
-      if (map is Map<String, dynamic>) return Product.fromJson(map);
-      if (map is List)
-        return map.map((item) => Product.fromJson(item)).toList();
-    };
-    httpClient.baseUrl = 'YOUR-API-URL';
-  }
+  String url =
+      'https://product-565e4-default-rtdb.asia-southeast1.firebasedatabase.app/';
 
-  Future<Product?> getProduct(int id) async {
-    final response = await get('product/$id');
+  Future<void> editProduct(String id, String name) async {
+    final response = await patch(url + 'products/$id.json', {"name": name});
     return response.body;
   }
 
-  Future<Response<Product>> postProduct(Product product) async =>
-      await post('product', product);
-  Future<Response> deleteProduct(int id) async => await delete('product/$id');
+  Future<dynamic> postProduct(String name, String date) async {
+    final response =
+        await post(url + 'products.json', {"name": name, "createdAt": date});
+    return response.body;
+  }
+
+  Future<void> deleteProduct(String id) async => await delete(
+        url + 'products/$id.json',
+      );
 }
